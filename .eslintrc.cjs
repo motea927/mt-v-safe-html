5b395898a3231a7e05d8b7d4887d4d7532ee8057
motea927 <motea927@yahoo.com.tw>
2023-06-09 01:05:32 +0800

    chore: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'eslint-config-unjs',
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'unicorn/filename-case': 0,
+    'unicorn/no-null': 0,
+    'import/named': 0
+  }
+}
