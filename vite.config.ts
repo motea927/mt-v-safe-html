7a84fe01f801953b2a65eeb53f994b0c9a125281
motea927 <motea927@yahoo.com.tw>
2023-06-09 17:34:31 +0800

    chore: make sure to externalize deps that shouldn't be bundled

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,18 @@ export default defineConfig({
       name: 'mt-v-safe-html',
       // the proper extensions will be added
       fileName: 'mt-v-safe-html'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
     }
   }
 })
