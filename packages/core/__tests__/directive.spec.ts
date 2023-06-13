8160dc39400f3a5b906b562fe3c9938094d89ad4
motea927 <motea927@yahoo.com.tw>
2023-06-13 09:55:16 +0800

    test: add more htmlString for global directive test

--- packages/core/__tests__/directive.spec.ts
+++ packages/core/__tests__/directive.spec.ts
@@ -2,9 +2,12 @@ import { describe, it, expect, beforeEach } from 'vitest'
 import { shallowMount } from '@vue/test-utils'
 import { createDirective } from '../directive'
 import { setGlobalOptions } from '../shared'
-import type { Options } from '../shared'
+import type { Options, BindingObj } from '../shared'
 
-const mountGlobalElement = (htmlString: string, globalOptions?: Options) => {
+const mountGlobalElement = (
+  htmlString: string | (() => string) | BindingObj,
+  globalOptions?: Options
+) => {
   const Component = {
     template: '<div v-safe-html="htmlString"></div>'
   }
@@ -28,28 +31,74 @@ beforeEach(() => {
 })
 
 describe('Global directive', () => {
-  it('Basic Global directive can work', () => {
+  describe('Basic Global directive htmlString can work', () => {
     const htmlString = `<p>this is html</p>`
-    const wrapper = mountGlobalElement(htmlString)
-    expect(wrapper.html()).toContain(htmlString)
+
+    it('pure string params', () => {
+      const wrapper = mountGlobalElement(htmlString)
+      expect(wrapper.html()).toContain(htmlString)
+    })
+
+    it('function string params', () => {
+      const wrapper = mountGlobalElement(() => htmlString)
+      expect(wrapper.html()).toContain(htmlString)
+    })
+
+    it('obj params', () => {
+      const wrapper = mountGlobalElement({ htmlString })
+      expect(wrapper.html()).toContain(htmlString)
+    })
   })
 
-  it('Global default string can work', () => {
+  describe('Global default string can work', () => {
     const notMatchHtmlString = '<div>something</div></div>'
     const matchHtmlString = '<div>something</div>'
     const defaultString = 'not match error'
-    const errorWrapper = mountGlobalElement(notMatchHtmlString, {
-      defaultString
-    })
-    const correctWrapper = mountGlobalElement(matchHtmlString, {
-      defaultString
+
+    it('pure string params', () => {
+      const errorWrapper = mountGlobalElement(notMatchHtmlString, {
+        defaultString
+      })
+      const correctWrapper = mountGlobalElement(matchHtmlString, {
+        defaultString
+      })
+
+      expect(errorWrapper.html()).toContain(defaultString)
+      expect(correctWrapper.html()).toContain(matchHtmlString)
     })
 
-    expect(errorWrapper.html()).toContain(defaultString)
-    expect(correctWrapper.html()).toContain(matchHtmlString)
+    it('function string params', () => {
+      const errorWrapper = mountGlobalElement(() => notMatchHtmlString, {
+        defaultString
+      })
+      const correctWrapper = mountGlobalElement(() => matchHtmlString, {
+        defaultString
+      })
+
+      expect(errorWrapper.html()).toContain(defaultString)
+      expect(correctWrapper.html()).toContain(matchHtmlString)
+    })
+
+    it('obj params', () => {
+      const errorWrapper = mountGlobalElement(
+        { htmlString: notMatchHtmlString },
+        {
+          defaultString
+        }
+      )
+      const correctWrapper = mountGlobalElement(
+        { htmlString: matchHtmlString },
+        {
+          defaultString
+        }
+      )
+
+      expect(errorWrapper.html()).toContain(defaultString)
+      expect(correctWrapper.html()).toContain(matchHtmlString)
+    })
   })
 
-  it('Global sanitize config can work', () => {
+  describe('Global sanitize config can work', () => {
     const htmlString = `
     <a
       rel="noopener noreferrer"
@@ -59,15 +108,42 @@ describe('Global directive', () => {
       link
     </a>
   `
-
-    const noConfigWrapper = mountGlobalElement(htmlString)
-
     const sanitizeConfig = { ADD_ATTR: ['target'] }
-    const configWrapper = mountGlobalElement(htmlString, {
-      sanitizeConfig
+
+    it('pure string params', () => {
+      const noConfigWrapper = mountGlobalElement(htmlString)
+
+      const configWrapper = mountGlobalElement(htmlString, {
+        sanitizeConfig
+      })
+
+      expect(noConfigWrapper.find('a').attributes('target')).toBeFalsy()
+      expect(configWrapper.find('a').attributes('target')).toContain('_blank')
     })
 
-    expect(noConfigWrapper.find('a').attributes('target')).toBeFalsy()
-    expect(configWrapper.find('a').attributes('target')).toContain('_blank')
+    it('function string params', () => {
+      const noConfigWrapper = mountGlobalElement(() => htmlString)
+
+      const configWrapper = mountGlobalElement(htmlString, {
+        sanitizeConfig
+      })
+
+      expect(noConfigWrapper.find('a').attributes('target')).toBeFalsy()
+      expect(configWrapper.find('a').attributes('target')).toContain('_blank')
+    })
+
+    it('obj params', () => {
+      const noConfigWrapper = mountGlobalElement(() => htmlString)
+
+      const configWrapper = mountGlobalElement(
+        { htmlString },
+        {
+          sanitizeConfig
+        }
+      )
+
+      expect(noConfigWrapper.find('a').attributes('target')).toBeFalsy()
+      expect(configWrapper.find('a').attributes('target')).toContain('_blank')
+    })
   })
 })
