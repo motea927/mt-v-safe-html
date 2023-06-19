11c781eab75128b62949b46af1efcc8b01c82f1a
motea927 <motea927@yahoo.com.tw>
2023-06-19 11:16:44 +0800

    fix: move ComponentCustomProperties to main

--- packages/core/types.ts
+++
@@ -1,7 +0,0 @@
-import type { BindingObj, BindingI18nObj } from './shared'
-declare module '@vue/runtime-core' {
-  export interface ComponentCustomProperties {
-    $i18nHtml: (binding: string | BindingI18nObj) => string
-    $safeHtml: (binding: string | BindingObj) => string
-  }
-}
